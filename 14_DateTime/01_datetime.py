cbba80ea229f7be8590e669c72c0c2fa654b52b7
Gena <99088678+GenaKafe@users.noreply.github.com>
2022-02-10 13:30:17 +0100

    Add files via upload

---
+++ 14_DateTime/01_datetime.py
@@ -0,0 +1,12 @@
+import datetime
+
+
+my_datetime = datetime.datetime.now()
+print(my_datetime) # 2021-10-25 16:09:11.848626
+
+
+my_day = datetime.date.today()
+print(my_day)
+
+
+# Check all availabale XXXXX
